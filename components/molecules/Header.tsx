c6e25ce96f8034eb006439502b405d8b39ebedf8
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-02-05 19:12:38 +0900

    create header

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { VFC } from 'react';
+import { css } from '@emotion/react';
+import { Flex, Text, Box } from '@chakra-ui/react';
 
-export const Header = () => {
+const header = css`
+  height: 64px;
+  box-shadow: 0px 4px 4px rgba(96, 134, 156, 0.25);
+`
+
+export const Header: VFC = () => {
   return(
     <>
-      <div></div>
+      <Flex css={header}>
+        <Box display='flex' alignItems='center' pl={10}>
+          <Text fontSize='2xl' fontFamily='-moz-initial'>Numa.blog</Text>
+        </Box>
+      </Flex>
     </>
   )
 }

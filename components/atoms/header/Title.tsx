320cd80947839127d4d55aadd223dee88283cf00
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-02-05 19:19:22 +0900

    fix: code style

--- components/atoms/header/Title.tsx
+++ components/atoms/header/Title.tsx
@@ -3,8 +3,12 @@ import { Box, Text } from '@chakra-ui/react'
 
 export const Title: VFC = () => {
   return(
-    <Box display='flex' alignItems='center' pl={10}>
-      <Text fontSize='2xl' fontFamily='-moz-initial' textColor='#666A71'>Numa.blog</Text>
-    </Box>
+    <>
+      <Box display='flex' alignItems='center' pl={10}>
+        <Text fontSize='2xl' fontFamily='-moz-initial' textColor='#666A71'>
+          Numa.blog
+        </Text>
+      </Box>
+    </>
   )
 }

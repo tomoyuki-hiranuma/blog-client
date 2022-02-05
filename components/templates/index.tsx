ae574529000cefa3484fae5e99e5de157cfce59c
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-02-05 18:15:22 +0900

    create base component

---
+++ components/templates/index.tsx
@@ -0,0 +1,12 @@
+import React, { VFC } from 'react'
+import { Layout } from '../organisms/Layout'
+
+export const IndexPage: VFC = () => {
+  return (
+    <Layout>
+      <div>test</div>
+    </Layout>
+  )
+}
+
+

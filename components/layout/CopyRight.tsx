d7a305482984b667035c6e2fb787b3bc0613dcf1
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-02-18 23:38:02 +0900

    fix: quotation

--- components/layout/CopyRight.tsx
+++ components/layout/CopyRight.tsx
@@ -1,10 +1,11 @@
 import React, { VFC } from 'react';
 import { Container } from '@chakra-ui/react';
+import { theme } from '../../styles/base/theme';
 
 export const CopyRight: VFC = () => {
   return(
     <>
-      <Container textColor={'#FFFFFF'} textAlign={'right'} fontSize={'sm'} pt={`4`}>
+      <Container textColor={theme.colors.white} textAlign={`right`} fontSize={`sm`} pt={4}>
         &copy;2021 Hiranuma, Tomoyuki
       </Container>
     </>
--- components/layout/HeaderTitle.tsx
+++ components/layout/HeaderTitle.tsx
@@ -6,9 +6,9 @@ export const HeaderTitle: VFC = () => {
   return(
     <>
       <Link href={`/`}>
-        <Box display='flex' alignItems='center' pl={10} cursor={'pointer'}>
+        <Box display={`flex`} alignItems={`center`} pl={10} cursor={`pointer`}>
           <a>
-            <Text fontSize='2xl' fontFamily='-moz-initial' textColor='#666A71'>
+            <Text fontSize={`2xl`} fontFamily={`cursive`}>
                 Numa.blog
             </Text>
           </a>

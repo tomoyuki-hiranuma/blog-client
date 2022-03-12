1f3d76dc16351bd69735d0d166af3553295fca51
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-03-12 14:25:21 +0900

    fix: remove comment

--- components/layout/CopyRight.tsx
+++ components/layout/CopyRight.tsx
@@ -5,7 +5,6 @@ import { theme } from '../../styles/base/theme';
 export const CopyRight: VFC = () => {
   return(
     <>
-      {/* Memo:背景と文字の色のコントラストが近いので適切にする． */}
       <Container textColor={theme.colors.white} textAlign={`right`} fontSize={`sm`} pt={4}>
         &copy;2021 Hiranuma, Tomoyuki
       </Container>

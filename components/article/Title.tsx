f55392e785e3af75934c8770a54f539887680cf0
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-03-12 13:38:27 +0900

    fix: bug

--- components/article/Title.tsx
+++ components/article/Title.tsx
@@ -5,14 +5,14 @@ import { Text, Flex, Box } from '@chakra-ui/react';
 export const Title: VFC = () => {
   const router = useRouter();
   const { id } = router.query;
-  
+
   return(
     <>
       <Flex align={`center`} minWidth={`280px`} gap={4}>
         <Box border={`0.5px`} borderStyle={`solid`} w={{ base: `40px`, md: `80px` }} />
         <Text fontSize={{ base: `2xl`, md: `4xl` }}>記事</Text>
         <Box border={`0.5px`} borderStyle={`solid`} w={{ base: `40px`, md: `80px` }} />
-        <Text fontSize={{ base: `xl`, md: `2xl` }}>{`${!!id ? `${id}編` : null}`}</Text>
+        <Text fontSize={{ base: `xl`, md: `2xl` }}>{`${!!id ? `${id}編` : ""}`}</Text>
       </Flex>
     </>
   );

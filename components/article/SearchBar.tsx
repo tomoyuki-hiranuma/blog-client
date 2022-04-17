417e4177c03bc20f587a62e8d62f8bd513bf146a
tomoyuki-hiranuma <numanumatom@gmail.com>
2022-04-17 18:26:10 +0900

    fix

--- components/article/SearchBar.tsx
+++ components/article/SearchBar.tsx
@@ -1,7 +1,10 @@
 import React, { VFC } from 'react';
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library, IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSearch as IconDefinition);
 
 export const SearchBar: VFC = () => {
   return(
@@ -9,7 +12,7 @@ export const SearchBar: VFC = () => {
       <InputGroup maxWidth={`250px`} width={`auto`}>
         <Input />
         <InputRightElement>
-          <FontAwesomeIcon icon={faSearch} height={`18px`} />
+          <FontAwesomeIcon icon={faSearch as IconDefinition} height={`18px`} />
         </InputRightElement>
       </InputGroup>
     </>
